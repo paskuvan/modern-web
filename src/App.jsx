5c358d1d73f192eac1e5ef442bcb1ca68343b1ee
paskuvan <majopaskuvan@gmail.com>
2025-03-08 00:10:57 -0300

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
+import "./App.css";
+import { LoadingScreen } from "./components/LoadingScreen";
 
 function App() {
- 
-  return (
-    <>
-      
-    </>
-  )
+ return (
+  <>
+   <LoadingScreen />
+  </>
+ );
 }
 
-export default App
+export default App;
