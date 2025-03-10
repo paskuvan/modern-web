49f8ef2096d43fb7f71b7ef19078d1ab6f2e455a
paskuvan <majopaskuvan@gmail.com>
2025-03-10 00:41:59 -0300

    loading ok

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ function App() {
   return (
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
+        {/* Your main content goes here */}
+      </div>
     </>
   );
 }
