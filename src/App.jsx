a8acec5c903222e67ea73df5a812dd94dead4946
paskuvan <majopaskuvan@gmail.com>
2025-03-09 21:54:11 -0300

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import "./App.css";
+import { useState } from "react";
 import { LoadingScreen } from "./components/LoadingScreen";
+import "./index.css";
+import "./App.css";
 
 function App() {
- return (
-  <>
-   <LoadingScreen />
-  </>
- );
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  return (
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+    </>
+  );
 }
 
 export default App;
