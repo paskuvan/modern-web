cdb592996f8eb3d2eb937ec61237dd3a8cb5f308
paskuvan <majopaskuvan@gmail.com>
2025-03-12 20:21:07 -0300

    text

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -9,6 +9,9 @@ export const Home = () => {
             <h1 className="text-5xl md:text-7xl font-bold mb-6  bg-gradient-to-r from-blue-500 to-purple-600  bg-clip-text text-transparent leading-right">
                 Hi, I'm Majo Paskuvan
             </h1>
+            <p className="text-gray-40 text-lg mb-8 max-w-lg mx-auto">
+               I'm a frontend developer based in Santiago, Chile, with a passion for creating beautiful and functional websites, applications, and everything in between, always looking to learn and improve my skills. 
+            </p>
         </div>
 
         </section>
