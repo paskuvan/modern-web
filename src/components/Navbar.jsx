853c9b39061b711631b08367e44fcf94df187ae0
paskuvan <majopaskuvan@gmail.com>
2025-03-12 00:01:11 -0300

    name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,12 @@ export const Navbar = () => {
     return (
          <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
        
-       <div>
-
+       <div className="max-w-5xl mx-auto px-4">
+        <div className="flex justify-between items-center h-16">
+            <a href="#home" className="font-mono text-xl font-bold text-white">
+                {""}
+                Majo <span className="text-purple-500">Paskuvan</span>clear</a>
+        </div>
        </div>
     
     </nav>
